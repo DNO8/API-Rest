6362534f604d95c9ab5edf981ace4fe6cb069d2f
DANO <dcontrerasl@live.com>
2025-05-10 23:05:06 -0400

    cambiar a MVC integrando modelo y controlador

---
+++ model/movie.js
@@ -0,0 +1,41 @@
+import movies from '../json/movies.json' with {type:"json"};
+import { randomUUID } from 'node:crypto';
+export class MovieModel {
+    static async getAll({genre}) 
+    {
+        if(genre)
+        {
+            return movies.filter(movie=>movie.genre.some(gen=gen.toLowerCase()===genre.toLowerCase()))
+        }
+        return movies;
+    }
+    static async getById({id})
+    {
+         const movie=movies.find(movie=> movie.id===id)
+         return movie
+    }
+    static async create ({input})
+    {
+        const newMovie={
+                id: randomUUID(),
+                ...input
+            }
+        movies.push(newMovie)
+    }
+    static async delete ({id}){
+        const movieIndex=movie.findIndex(movie=>movie.id===id)
+        if(movieIndex===-1) return false
+        movie.splice(movieIndex,1);
+        return true
+    }
+    static async update ({input, id})
+    {
+        const movieIndex=movies.findIndex(movie=> movie.id===id)
+        if(movieIndex===-1) return false
+        const updateMovie={
+            ...movies[movieIndex],
+            ...input,
+        }  
+        return updateMovie  
+    }
+}
